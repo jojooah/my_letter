b0b188bb29d448dddf1bd37b9f6517917fd84b09
jojooah <my_sg1@naver.com>
2024-05-20 12:39:25 +0900

    Initial commit

---
+++ src/main/java/com/jojo/my_letter/MyLetterApplication.java
@@ -0,0 +1,13 @@
+package com.jojo.my_letter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLetterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyLetterApplication.class, args);
+    }
+
+}
