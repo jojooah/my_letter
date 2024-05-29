90362fe843364b6ccdb10e3f9110acc4f14d44ab
jojooah <my_sg1@naver.com>
2024-05-30 00:55:58 +0900

    부트스트랩

--- src/main/java/com/jojo/my_letter/api/HealthCheckApi.java
+++ src/main/java/com/jojo/my_letter/api/HealthCheckApi.java
@@ -1,14 +1,14 @@
 package com.jojo.my_letter.api;
 
 import com.jojo.my_letter.model.result.RestResult;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.util.LinkedHashMap;
 import java.util.Map;
-
-@RestController
+@Controller
 public class HealthCheckApi {
 
     // 누가 쓸까요? AWS ELB에서 헬스체크 api 로 활용합니다.
@@ -19,5 +19,17 @@ public class HealthCheckApi {
         data.put("version", "0.0.1");
         data.put("timestamp", LocalDateTime.now());
         return new RestResult(data);
+    }
+
+    // 누가 쓸까요? AWS ELB에서 헬스체크 api 로 활용합니다.
+    @GetMapping("/index")
+    public String healthCheck1() {
+
+        return "index";
+    }
+    @GetMapping("/newsletter/item")
+    public String newsletterItem() {
+
+        return "home";
     }
 }
