bb80d47f53d6ded665426b87434de8fd7a27d804
jojooah <my_sg1@naver.com>
2024-05-31 03:21:24 +0900

    view controller 생성

--- src/main/java/com/jojo/my_letter/api/HealthCheckApi.java
+++ src/main/java/com/jojo/my_letter/api/HealthCheckApi.java
@@ -17,7 +17,7 @@ import java.util.Map;
 //todo
 // 백엔드 코드를 더 올려주시면 살펴보겠습니다 :)
 // 제 생각에는 우선 만약에 코드 내에서 예외가 발생하는 경우를 어떻게 처리할 수 있는지를 고민해보셔서 올려주시면 좋을 것 같습니다.
-@Controller
+@RestController
 public class HealthCheckApi {
 
     // 누가 쓸까요? AWS ELB에서 헬스체크 api 로 활용합니다.
@@ -28,48 +28,5 @@ public class HealthCheckApi {
         data.put("version", "0.0.1");
         data.put("timestamp", LocalDateTime.now());
         return new RestResult(data);
-    }
-
-    // 누가 쓸까요? AWS ELB에서 헬스체크 api 로 활용합니다.
-    @GetMapping("/index")
-    public String healthCheck1() {
-
-        return "index";
-    }
-
-    @GetMapping("/newsletter/item")
-    public String newsletterItem() {
-
-        // fixme 하실 계획이셨는지는 모르겠으나 웬만하면 하나의 파일보다는 페이지 구성을 fragment 를 활용해서 역할별로 header, footer, body 등으로 나누어서 구성하는게 좋습니다.
-        return "shop";
-    }
-
-    @GetMapping("/newsletter/item/1")
-    public String newsletterItem1() {
-
-        return "shop-detail";
-    }
-
-    @GetMapping("/weeks/monday")
-    public String monday() {
-
-        return "weeks";
-    }
-
-    @GetMapping("/checkout")
-    public String checkout() {
-
-        return "chackout";
-    }
-
-    @GetMapping("/mypage")
-    public String mypage() {
-
-        return "myPage";
-    }
-    @GetMapping("/scrap")
-    public String scrap() {
-
-        return "scrap";
     }
 }
