8b2d8bf66f5576a9ca8133dcaf683aeac434e43c
yoe21c <yoe21c@gmail.com>
2024-06-22 22:00:21 +0900

    * 금일자 내용 푸시합니다.

--- src/main/java/com/jojo/my_letter/controller/page/LoginPage.java
+++ src/main/java/com/jojo/my_letter/controller/page/LoginPage.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import static com.jojo.my_letter.utils.Utils.toJson;
+
 @Slf4j
 @Controller
 @RequiredArgsConstructor
@@ -20,6 +22,13 @@ public class LoginPage {
 
     private final LoginService loginService;
 
+    /**
+     * 로그인 페이지
+     * @param model 로그인 정보
+     * @param error 에러 메시지
+     * @param session 에러 메시지
+     * @return
+     */
     @GetMapping("/login")
     public String login(Model model, @RequestParam(required = false) String error, HttpSession session) {
         if (error != null) {
@@ -29,6 +38,12 @@ public class LoginPage {
         return "login";
     }
 
+    /**
+     * 회원가입 페이지
+     * @param model 회원가입 정보
+     * @param error 에러 메시지
+     * @return
+     */
     @GetMapping("/join")
     public String showJoinForm(Model model, @RequestParam(required = false) String error) {
         if (!model.containsAttribute("member")) {
@@ -40,28 +55,25 @@ public class LoginPage {
         return "join";
     }
 
+    /**
+     * 회원가입
+     * todo 1. 자동로그인 안시켜주는 정책. (선택)
+     * todo 2. 자동로그인 시켜준다면, 로그인을 시켜주고 메인페이지로 리다이렉트 시켜주는 정책. (선택했으면 좋겠습니다.)
+     * @param member 회원가입 정보
+     * @param redirectAttributes 리다이렉트 시킬 때, 데이터를 넘겨주는 객체
+     * @return
+     */
     @PostMapping("/join")
     public String join(Member member, RedirectAttributes redirectAttributes) {
         try {
-            if (loginService.join(member)) {
-                return "redirect:/login";
-            }else{
-                redirectAttributes.addFlashAttribute("errorMessage","회원가입에 실패했습니다.");
-                redirectAttributes.addFlashAttribute("member", member);
-            }
-        } catch (RestErrorException e) {
-            log.error(e.getRestError().getMessage());
-            redirectAttributes.addFlashAttribute("errorMessage", e.getRestError().getMessage());
-            redirectAttributes.addFlashAttribute("member", member);
-            return "redirect:/join";
+            final Member joined = loginService.join(member);
+            log.info("회원가입 성공 : {}, redirect:/login", toJson(joined));
+            return "redirect:/login";
         } catch (Exception e) {
-            log.error(e.getMessage());
             redirectAttributes.addFlashAttribute("errorMessage", e.getMessage());
             redirectAttributes.addFlashAttribute("member", member);
+            log.error("회원가입 실패 : {}, member: {}, redirect:/join", e.getMessage(), toJson(member));
             return "redirect:/join";
         }
-        redirectAttributes.addFlashAttribute("errorMessage", "에러 발생. 관리자에게 문의하세요.");
-        redirectAttributes.addFlashAttribute("member", member);
-        return "redirect:/join";
     }
 }
