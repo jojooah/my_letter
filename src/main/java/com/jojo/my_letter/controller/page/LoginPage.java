bc37d6300c7e7f7f6a54bf168b1ff9b61545d948
jojooah <my_sg1@naver.com>
2024-06-22 17:45:16 +0900

    회원가입

--- src/main/java/com/jojo/my_letter/controller/page/LoginPage.java
+++ src/main/java/com/jojo/my_letter/controller/page/LoginPage.java
@@ -1,10 +1,13 @@
 package com.jojo.my_letter.controller.page;
 
+import com.jojo.my_letter.controller.service.impl.LoginService;
+import com.jojo.my_letter.model.entity.Member;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Slf4j
@@ -12,14 +15,35 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequiredArgsConstructor
 public class LoginPage {
 
+    private final LoginService loginService;
+
     @GetMapping("/login")
     public String login(Model model, @RequestParam(required = false) String error) {
-
-        if(error != null && error.equalsIgnoreCase("invalidUser")) {
-            model.addAttribute("errorMessage", "누구시죠???");
+        if (error != null) {
+            model.addAttribute("errorMessage", error);
         }
-
         return "login";
     }
 
+
+    @GetMapping("/join")
+    public String join(Model model,@RequestParam(required = false) String error) {
+        if (error != null) {
+            model.addAttribute("errorMessage", error);
+        }
+        return "join";
+    }
+
+    @PostMapping("/join")
+    public String join(Member member) {
+        try {
+            if(loginService.join(member)){
+               return "redirect:/login";
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage());
+           return "redirect:/join?error="+e.getMessage();
+        }
+        return "redirect:/join?error=에러발생. 관리자에게 문의하세요";
+    }
 }
--- src/main/java/com/jojo/my_letter/mapper/MemberMapper.java
+++ src/main/java/com/jojo/my_letter/mapper/MemberMapper.java
@@ -6,7 +6,7 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface MemberMapper {
 
-    Member findMember(String name);
-
+    Member findMember(String id);
     void updatePassword(String name, String encodedPassword);
+    int insertMember(Member member);
 }
