1ac29c5f1bb6f9703e074858f19906aec6b11e04
jojooah <my_sg1@naver.com>
2024-07-24 21:19:33 +0900

    회원가입시 로그인

--- src/main/java/com/jojo/my_letter/controller/page/LoginController.java
+++ src/main/java/com/jojo/my_letter/controller/page/LoginController.java
@@ -1,31 +1,35 @@
 package com.jojo.my_letter.controller.page;
 
+import com.jojo.my_letter.config.security.LoginSuccessHandler;
 import com.jojo.my_letter.controller.service.impl.LoginService;
 import com.jojo.my_letter.model.entity.Member;
 import com.jojo.my_letter.model.result.RestErrorException;
 import jakarta.servlet.http.HttpSession;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import java.util.stream.Collectors;
 
 import static com.jojo.my_letter.utils.Utils.toJson;
 
 @Slf4j
 @Controller
 @RequiredArgsConstructor
-public class LoginPage {
+public class LoginController {
 
     private final LoginService loginService;
+    private final AuthenticationManager authenticationManager; // AuthenticationManager 주입
 
     /**
      * 로그인 페이지
@@ -63,26 +67,39 @@ public class LoginPage {
 
     /**
      * 회원가입
-     * todo 1. 자동로그인 안시켜주는 정책. (선택)
-     * todo 2. 자동로그인 시켜준다면, 로그인을 시켜주고 메인페이지로 리다이렉트 시켜주는 정책. (선택했으면 좋겠습니다.)
      * @param member 회원가입 정보
      * @param redirectAttributes 리다이렉트 시킬 때, 데이터를 넘겨주는 객체
      * @return
      */
     @PostMapping("/join")
-    public String join(@Validated @ModelAttribute Member member, BindingResult bindingResult,RedirectAttributes redirectAttributes) {
+    public String join(@Validated @ModelAttribute Member member, BindingResult bindingResult,RedirectAttributes redirectAttributes,HttpSession session) {
         if (bindingResult.hasErrors()) {
             return "/join";
         }
         try {
+            String unEncodedPwd = member.getPassword(); // 나중에 인코딩되어서 비교
             final Member joined = loginService.join(member);
-            log.info("회원가입 성공 : {}, redirect:/login", toJson(joined));
-            return "redirect:/login";
+
+            // 자동 로그인 처리
+            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(joined.getId(), unEncodedPwd);
+            Authentication authentication = authenticationManager.authenticate(authToken);
+            session.setAttribute("FROM_JOIN_PAGE", true);
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            log.info("회원가입 성공 : {}, redirect:/", toJson(joined));
+            return "redirect:/";
+        }
+        catch (RestErrorException e){
+            redirectAttributes.addFlashAttribute("errorMessage", e.getRestError().getMessage());
+            redirectAttributes.addFlashAttribute("member", member);
+            log.error("회원가입 실패1 : {}, member: {}, redirect:/join", e.getRestError().getMessage(), toJson(member));
+            return "redirect:/join";
         }
         catch (Exception e) {
             redirectAttributes.addFlashAttribute("errorMessage", e.getMessage());
             redirectAttributes.addFlashAttribute("member", member);
-            log.error("회원가입 실패 : {}, member: {}, redirect:/join", e.getMessage(), toJson(member));
+            log.error("회원가입 실패2 : {}, member: {}, redirect:/join", e.getMessage(), toJson(member));
             return "redirect:/join";
         }
     }
--- src/main/java/com/jojo/my_letter/exception/GlobalExceptionHandler.java
+++ src/main/java/com/jojo/my_letter/exception/GlobalExceptionHandler.java
@@ -7,14 +7,9 @@ import com.jojo.my_letter.service.thirdparty.TelegramService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import java.util.*;
 
 /**
  * 스프링시큐리티에서 처리해야함: 401(인증에러), 403(인가에러)
@@ -36,27 +31,15 @@ public class GlobalExceptionHandler  {
         return new RestError(re.getRestError().getCode(), re.getMessage());
     }
 
-    /*@ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    protected RestError validationHandleException(MethodArgumentNotValidException ex){
-        String errorMessage = ex.getBindingResult()
-                .getFieldErrors()
-                .stream()
-                .findFirst()
-                .map(FieldError::getDefaultMessage)
-                .orElse("Unknown error");
-
-        return new RestError(RestErrorCode.ERROR_ETC.getCode(), errorMessage);
-    }
-
     // 서버 오류.
     @ExceptionHandler({Exception.class})
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     protected RestError handleException(Exception ex) {
         //텔레그램으로 메시지 보내기
         log.error(ex.getMessage());
-     //   telegramService.sendTelegram("ServerError! " + ex.getMessage());
+        telegramService.sendTelegram("ServerError! " + ex.getMessage());
         return new RestError(RestErrorCode.ERROR_ETC.getCode(), RestErrorCode.ERROR_ETC.getMessage());
-    }*/
+    }
+
 
 }
