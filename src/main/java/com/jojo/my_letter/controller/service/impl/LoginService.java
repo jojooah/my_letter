c91e0e5983d7fbccb392107ed17fc0ebdd830549
jojooah <my_sg1@naver.com>
2024-07-29 18:16:40 +0900

    로그인/로그아웃 로그남기기

--- src/main/java/com/jojo/my_letter/controller/service/impl/LoginService.java
+++ src/main/java/com/jojo/my_letter/controller/service/impl/LoginService.java
@@ -3,16 +3,11 @@ package com.jojo.my_letter.controller.service.impl;
 import com.jojo.my_letter.mapper.MemberMapper;
 import com.jojo.my_letter.model.entity.Member;
 import com.jojo.my_letter.model.result.RestErrorException;
-import io.micrometer.common.util.StringUtils;
-import jakarta.validation.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.validation.BindingResult;
-
-import java.util.Set;
 
 import static com.jojo.my_letter.model.result.RestErrorCode.*;
 import static io.micrometer.common.util.StringUtils.isBlank;
@@ -25,32 +20,25 @@ public class LoginService {
     private final PasswordEncoder bCryptPasswordEncoder;
 
     @Transactional
-    public Member join(@Valid Member member) {
-     /*   if (isBlank(member.getName())) throw  new RestErrorException(UNKNOWN_ERROR);
-        if (isBlank(member.getEmail())) throw  new RestErrorException(NEED_EMAIL);
+    public Member join(Member member) {
+        if (isBlank(member.getName())) throw new RestErrorException(UNKNOWN_ERROR);
+        if (isBlank(member.getEmail())) throw new RestErrorException(NEED_EMAIL);
         if (isBlank(member.getId())) throw new RestErrorException(NEED_USER_ID);
         if (isBlank(member.getPassword())) throw new RestErrorException(NEED_PWD);
         if (isBlank(member.getPasswordCheck())) throw new RestErrorException(NEED_NEW_PWD_FOR_CHECK);
-        if (!member.getPassword().equals(member.getPasswordCheck())) throw new  RestErrorException(WRONG_PWD);
+        if (!member.getPassword().equals(member.getPasswordCheck())) throw new RestErrorException(WRONG_PWD);
         if (isBlank(member.getUsername())) throw new RestErrorException(NEED_NICK_NAME);
         if (member.getPassword().length() < 4) throw new RestErrorException(UNKNOWN_ERROR);
-        if (memberMapper.findMember(member.getId())!=null)  throw new RestErrorException(USER_ID_ALREADY_EXIST);
-*/
-
-        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-        Validator validator = factory.getValidator();
-        Set<ConstraintViolation<Member>> violations = validator.validate(member);
+        if (memberMapper.findMember(member.getId()) != null) throw new RestErrorException(USER_ID_ALREADY_EXIST);
 
         String rawPwd = member.getPassword();
         String encPwd = bCryptPasswordEncoder.encode(rawPwd);
         member.setPassword(encPwd);
 
-        if(memberMapper.insertMember(member) < 1) {
+        if (memberMapper.insertMember(member) < 1) {
             throw new RestErrorException(UNKNOWN_ERROR);
         }
 
         return memberMapper.findMember(member.getId());
     }
-
-
 }
