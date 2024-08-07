4ec7767d54827cb1abb0c381ab0f478b96949e3f
jojooah <my_sg1@naver.com>
2024-08-07 23:54:05 +0900

    자잘한 오류 수정

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,17 +1,5 @@
 (function ($) {
     "use strict";
-
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner(0);
-
-
     // Fixed Navbar
     $(window).scroll(function () {
         if ($(window).width() < 992) {
