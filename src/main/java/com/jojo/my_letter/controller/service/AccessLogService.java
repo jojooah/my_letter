92d6c8e27ff5f53f3332e967994d2ff284d34c2b
jojooah <my_sg1@naver.com>
2024-08-24 20:15:52 +0900

    Async 적용

--- src/main/java/com/jojo/my_letter/controller/service/AccessLogService.java
+++ src/main/java/com/jojo/my_letter/controller/service/AccessLogService.java
@@ -4,6 +4,7 @@ import com.jojo.my_letter.mapper.AccessLogMapper;
 import com.jojo.my_letter.model.entity.AccessLog;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Service;
 public class AccessLogService {
     private final AccessLogMapper accessLogMapper;
 
+    @Async
     public void saveAceessLog(AccessLog accessLog) {
         accessLogMapper.insertAccessLog(accessLog);
     }
